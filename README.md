0c4b8f5655a2cc50b191bb33ae04778f8ca0b08e
Armando Ruiz <artmx@proton.me>
2025-11-23 23:28:19 -0600

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# Ruizamdev Portfolio
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  base: "/ruizamdev-portfolio/",
-  plugins: [tailwindcss()],
-});
